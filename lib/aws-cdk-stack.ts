27db0d6f8427d19b8ea26d35445b9aa7f8d86d90
varunmuriyanat <varunmuriyanat@gmail.com>
2025-08-11 13:02:41 -0400

    Added glue database, glue service role and a glue crawler

--- lib/aws-cdk-stack.ts
+++ lib/aws-cdk-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import * as kms from 'aws-cdk-lib/aws-kms';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as glue from 'aws-cdk-lib/aws-glue';
 import { Construct } from 'constructs';
  
 export class AwsCdkStack extends cdk.Stack {
@@ -16,6 +17,36 @@ export class AwsCdkStack extends cdk.Stack {
     });
  
     s3Bucket.grantRead(new iam.AccountRootPrincipal());
+
+    // Glue database
+    const glueDatabase = new glue.CfnDatabase(this, 'MyGlueDatabase', {
+      catalogId: this.account,
+      databaseInput: {
+        name: 'landing_db'
+      }
+    });
+
+    // Glue service role
+    const glueRole = new iam.Role(this, 'GlueCrawlerRole', {
+      assumedBy: new iam.ServicePrincipal('glue.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AWSGlueServiceRole')
+      ]
+    });
+    s3Bucket.grantRead(glueRole);
+
+    // Glue crawler
+    new glue.CfnCrawler(this, 'MyGlueCrawler', {
+      role: glueRole.roleArn,
+      databaseName: glueDatabase.ref,
+      targets: {
+        s3Targets: [
+          { path: `s3://${s3Bucket.bucketName}/` }
+        ]
+      },
+      name: 'input-files-crawler'
+    });
+
   }
 }
  
