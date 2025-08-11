482ad23a09660c0d288d0a90691625e3d7b2c2e5
varunmuriyanat <varunmuriyanat@gmail.com>
2025-08-10 23:28:52 -0400

    updated delete script

--- empty_and_delete_all_buckets.sh
+++ empty_and_delete_all_buckets.sh
@@ -11,3 +11,9 @@ aws s3api delete-objects --bucket $bucket --delete "$(echo $versions | jq '{Obje
 # Delete the bucket
 aws s3api delete-bucket --bucket $bucket
 
+# Check if the bucket is deleted
+if ! aws s3api head-bucket --bucket $bucket 2>/dev/null; then
+  echo "Bucket $bucket deleted successfully."
+else
+  echo "Bucket $bucket still exists."
+fi
